644f32002efe21dc9cb11e5ac2f45089f8b9ca16
Francisco Marasco quiroga <francisco.marasco@gmail.com>
2018-05-03 21:52:35 -0300

    Update

--- resources/views/admin/create-image.blade.php
+++ resources/views/admin/create-image.blade.php
@@ -12,19 +12,19 @@
 
 			<div class="form-group">
 			    <label>Imagen</label>
-			    <input type="text" name="files" class="form-control" value="">
+			    <input type="text" name="files" class="form-control" placeholder="http://marascoquirogaprop.com.ar/uploads/YU14XkkbsebJMyB0P65k.jpg">
 			</div>
 			<div class="form-group">
 			    <label>Ubicacion</label>
-			    <input type="text" name="ubicacion" class="form-control" value="">
+			    <input type="text" name="ubicacion" class="form-control" placeholder="Castelar Norte">
 			</div>
 			<div class="form-group">
 			    <label>Precio</label>
-			    <input type="text" name="precio" class="form-control" value="">
+			    <input type="text" name="precio" class="form-control" placeholder="USD 140.000">
 			</div>
 			<div class="form-group">
 			    <label>Descripcion</label>
-			    <textarea rows="3" name="descripcion" class="form-control"></textarea>
+			    <textarea rows="3" name="descripcion" class="form-control" placeholder="Cerca de la Estación, buena oportunidad de inversión"></textarea>
 			</div>
 			 <div class="form-group">
 			    <button type="submit"  class="btn btn-green btn-lg" value="generar">Generar Imagen</button>
